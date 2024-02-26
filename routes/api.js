c8f5367a50ea5a53b942596d2474f29d917c6418
Almer Tampus <almerflorestampus@gmail.com>
2024-02-26 15:20:20 +0800

    feat: integrate issue controller

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,23 @@
 'use strict';
 
-module.exports = function (app) {
+// controllers
+const IssueController = require('../controllers/issueController');
+const issueController = new IssueController();
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .post(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .put(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .delete(function (req, res){
-      let project = req.params.project;
-      
-    });
-    
+module.exports = function (app) {
+	app
+		.route('/api/issues/:project')
+		.get(issueController.read)
+		.post(issueController.create)
+		.put(async function (req, res) {
+			const { project } = req?.params;
+
+			res.status(200).json({ message: 'Good job' });
+		})
+
+		.delete(async function (req, res) {
+			const { project } = req?.params;
+
+			res.status(200).json({ message: 'Good job' });
+		});
 };
