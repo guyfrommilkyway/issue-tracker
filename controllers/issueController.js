5f9afeaae62233750a7ec7b2b536bfa159cb31cf
Almer Tampus <almerflorestampus@gmail.com>
2024-05-04 01:09:03 +0800

    feat: add query on read; clean code

--- controllers/issueController.js
+++ controllers/issueController.js
@@ -3,6 +3,9 @@ const IssueServices = require('../services/issue');
 const ProjectServices = require('../services/project');
 
 const ERROR_CONTROLLER = require('../constants/controller');
+const REQUIRED_FIELDS = require('../constants/requiredFields');
+const objectValidator = require('../utils/objectValidator');
+const createPayload = require('../utils/createPayload');
 
 const issueServices = new IssueServices();
 const projectServices = new ProjectServices();
@@ -16,26 +19,14 @@ class IssueController {
 			return;
 		}
 
-		const { issue_title, issue_text, created_by, assigned_to, status_text } =
-			req.body;
-
-		if (!issue_title || !issue_text || !created_by) {
-			console.log(ERROR_CONTROLLER.MISSING_FIELDS);
+		if (!objectValidator(req?.body, REQUIRED_FIELDS)) {
 			res.send(ERROR_CONTROLLER.MISSING_FIELDS);
 			return;
 		}
 
-		const payload = {
-			issue_title: issue_title?.toString()?.trim(),
-			issue_text: issue_text?.toString()?.trim(),
-			created_by: created_by?.toString()?.trim(),
-			assigned_to: assigned_to?.toString()?.trim(),
-			status_text: status_text?.toString()?.trim(),
-		};
+		const payload = createPayload(req?.body);
 
-		const { name, issues } =
-			(await projectServices.read(project)) ??
-			(await projectServices.create(project));
+		const { name, issues } = (await projectServices.read(project)) ?? (await projectServices.create(project));
 		const issue = await issueServices.create(payload);
 		await projectServices.update(name, [...issues, issue._id]);
 
@@ -44,13 +35,14 @@ class IssueController {
 
 	async read(req, res) {
 		const { project } = req?.params;
+		const query = req?.query;
 
 		if (!project) {
 			res.send(ERROR_CONTROLLER.MISSING_ID);
 			return;
 		}
 
-		const resProject = await projectServices.read(project);
+		const resProject = await projectServices.read(project, query);
 
 		if (!resProject) {
 			res.send(ERROR_CONTROLLER.NOT_FOUND);
