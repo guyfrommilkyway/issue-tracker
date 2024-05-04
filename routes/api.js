48e7b0192ae8bda36d237590ac91f1d8f6494271
Almer Tampus <almerflorestampus@gmail.com>
2024-05-04 21:12:54 +0800

    feat: handle put and delete requests

--- routes/api.js
+++ routes/api.js
@@ -9,15 +9,6 @@ module.exports = function (app) {
 		.route('/api/issues/:project')
 		.get(issueController.read)
 		.post(issueController.create)
-		.put(async function (req, res) {
-			const { project } = req?.params;
-
-			res.status(200).json({ message: 'Good job' });
-		})
-
-		.delete(async function (req, res) {
-			const { project } = req?.params;
-
-			res.status(200).json({ message: 'Good job' });
-		});
+		.put(issueController.update)
+		.delete(issueController.delete);
 };
